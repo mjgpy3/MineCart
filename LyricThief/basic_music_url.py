6a613c5988c1771c6be5528809ff4f48d9fe73c3
Michael Gilliland <mjg.py3@gmail.com>
2012-11-12 17:05:08 -0500

    Constructed an 'interface' for URLs and one for BasicMusicURLs

--- LyricThief/basic_music_url.py
+++ LyricThief/basic_music_url.py
@@ -8,4 +8,13 @@
 import url
 
 class BasicMusicUrl(url.Url):
-    pass
+    def __init__(self, base='', separator='-', extension='.html', artist='', song=''):
+        url.Url.__init__(self)
+
+        self.base = base
+        self.separator = separator
+        self.extension = extension
+        self.artist = artist.lower()
+        self.song = song.lower()
+
+
