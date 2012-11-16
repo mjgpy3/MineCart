a4a39b6d0156a4b574aaa011cb7efb913a355436
Michael Gilliland <mjg.py3@gmail.com>
2012-11-16 11:09:48 -0500

    Worked on adding Save/Save As/Exit functionality

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ MineCart
 ========
 
 A by need/want data-miner. 
+
+Build in Python as a pet-project.
--- Windowing/lyric_window.py
+++ Windowing/lyric_window.py
@@ -14,6 +14,45 @@ from glade_window import GladeWindow
 class LyricWindow(GladeWindow):
     def __init__(self): 
         GladeWindow.__init__(self, './GladeFiles/LyricWindow.glade')
+        self.output_file = None
+    
         self.tvi_lyrics = self.w_tree.get_widget('tviLyrics')
         self.window = self.connect_widget_by_name('wdwMain', 'destroy', lambda x: gtk.main_quit())
 
+        menu_bar = self.w_tree.get_widget('menBar')
+
+        file_tab = gtk.MenuItem('File')
+        file_tab.show()
+
+        sub_menu = gtk.Menu()
+
+        for option_name in ['Save', 'Save As', 'Exit']:
+            item = gtk.MenuItem(option_name)
+            sub_menu.append(item)
+            item.connect('activate', self.file_activated, option_name)
+            item.show()
+
+        file_tab.set_submenu(sub_menu)
+        sub_menu.show()
+        
+        menu_bar.append(file_tab)
+        self.window.show()
+        
+
+    def file_activated(self, sender, name):
+        print "In file_activated: ", name
+        if name == 'Exit':
+            gtk.main_quit()
+            self.window.hide()
+        elif name == "Save" and self.output_file == None:
+            self.save_as(sender)
+        elif name == "Save" and self.output_file:
+            self.save_existing(sender)
+        elif name == "Save As":
+            self.save_as(sender)
+
+    def save_existing(self, sender):
+        print "In save_existing"
+
+    def save_as(self, sender):
+        print "In save_as"
