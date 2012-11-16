e84988febdbd132d0a00046773e8bc491eb3de34
Michael Gilliland <mjg.py3@gmail.com>
2012-11-16 18:32:03 -0500

    Made a builder factory for sources

---
+++ LyricThief/Sources/builder_of_sources.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Created by Michael Gilliland
+# Date: Fri Nov 16 18:19:07 EST 2012
+# 
+# 
+
+from source_azlyrics import SourceAzlyrics
+from source_lyric_depot import SourceLyricDepot
+
+class BuilderOfSources:
+    def __init__(self):
+        self.sources = {'azlyrics': SourceAzlyrics, 'lyricdepot': SourceLyricDepot}
+
+    def get_names(self):
+        return list(self.sources)
+
+    def build_from_source(self, source_name, artist, song):
+        if source_name in self.sources:
+            return self.sources[source_name](artist, song)
+        else:
+            raise Exception('Source cannot be built!')
+
+
+if __name__ == '__main__':
+    b = BuilderOfSources()
+
+    print b.get_names()
+
+    s = b.build_from_source('lyricdepot', 'Counting Crows', 'Omaha')
+
+    s.build_url()
+    print s.get_lyrics()
