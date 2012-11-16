e6743c4c7fcb386751575dd35dd3a7df3520ed00
Michael Gilliland <mjg.py3@gmail.com>
2012-11-16 13:39:03 -0500

    Made the 'Save' and 'Save As...' options work

--- Tools/clean.sh
+++ Tools/clean.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-n_pyc=0
-n_swp=0
+find .. -name "*.pyc" | xargs rm &> /dev/null
+find .. -name "*.swp" | xargs rm &> /dev/null
 
-find .. -name "*.pyc" | xargs rm &> /dev/null && n_pyc=$(($n_pyc+1))
-find .. -name "*.swp" | xargs rm &> /dev/null && n_swp=$(($n_swp+1))
-
-echo "Number of PYC: $n_pyc"
-echo "Number of SWP: $n_swp"
--- Windowing/lyric_window.py
+++ Windowing/lyric_window.py
@@ -15,10 +15,11 @@ class LyricWindow(GladeWindow):
     def __init__(self, title): 
         GladeWindow.__init__(self, './GladeFiles/LyricWindow.glade')
         self.output_file = None
+        self.title = title
     
         self.tvi_lyrics = self.w_tree.get_widget('tviLyrics')
         self.window = self.connect_widget_by_name('wdwMain', 'destroy', lambda x: gtk.main_quit())
-        self.window.set_title(title)
+        self.window.set_title(self.title)
 
         menu_bar = self.w_tree.get_widget('menBar')
 
@@ -27,7 +28,7 @@ class LyricWindow(GladeWindow):
 
         sub_menu = gtk.Menu()
 
-        for option_name in ['_Save', 'Save _As', '_Exit']:
+        for option_name in ['_Save', 'Save _As...', '_Exit']:
             item = gtk.MenuItem(option_name)
             sub_menu.append(item)
             item.connect('activate', self.file_activated, option_name)
@@ -41,7 +42,6 @@ class LyricWindow(GladeWindow):
         
 
     def file_activated(self, sender, name):
-        print "In file_activated: ", name
         if name == '_Exit':
             gtk.main_quit()
             self.window.hide()
@@ -49,11 +49,30 @@ class LyricWindow(GladeWindow):
             self.save_as(sender)
         elif name == "_Save" and self.output_file:
             self.save_existing(sender)
-        elif name == "Save _As":
+        elif name == "Save _As...":
             self.save_as(sender)
 
     def save_existing(self, sender):
-        print "In save_existing"
+        txt_buffer = self.tvi_lyrics.get_buffer()
+        start = txt_buffer.get_start_iter()
+        end = txt_buffer.get_end_iter()
+
+        with open(self.output_file, 'w') as f:
+            f.write(txt_buffer.get_text(start, end))
 
     def save_as(self, sender):
-        print "In save_as"
+        chooser = gtk.FileChooserDialog(title='Save As...', action=gtk.FILE_CHOOSER_ACTION_SAVE,
+                                        buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
+        chooser.set_current_name(self.title+'.txt')
+        chooser.set_default_response(gtk.RESPONSE_OK)
+        a_filter = gtk.FileFilter()
+        a_filter.set_name('Text File')
+        a_filter.add_pattern("*.txt")
+        chooser.add_filter(a_filter)
+
+        response = chooser.run()
+
+        if response == gtk.RESPONSE_OK:
+            self.output_file = chooser.get_filename()
+            self.save_existing(sender)
+        chooser.destroy() 
