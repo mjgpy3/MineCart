1eb2163b16223329f5bd4cc900852667a7d2a805
Michael Gilliland <mjg.py3@gmail.com>
2013-01-15 22:36:07 -0500

    Added a writer superclass and one child

--- EmailGrabber/email_grabber.py
+++ EmailGrabber/email_grabber.py
@@ -12,3 +12,19 @@ def get_email_from_url(url):
     reader = urllib2.urlopen(url)
     emails = set(re.findall('[[\w|\.]+\@\w+\.\w{3}', reader.read()))
     return emails
+
+class EmailFileWriter:
+    def __init__(self, name):
+        self.output_name = name
+
+    def write(self, emails, output_file_name):
+        raise NotImplementedError("Each writer must be able to write!")
+
+class CsvWriter(EmailFileWriter):
+    def __init__(self):
+        EmailFileWriter.__init__(self, "CSV")
+
+    def write(self, emails, output_file_name):
+        with open(output_file_name, 'w') as f:
+            for email in emails:
+                f.write(email + ',\n')
