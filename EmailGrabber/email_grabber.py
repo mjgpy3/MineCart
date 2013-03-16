4dfeff5fbf90cf8812d2fb5f1452e9cf75d7eaa8
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-15 21:05:19 -0400

    PyLint Cont...

--- EmailGrabber/email_grabber.py
+++ EmailGrabber/email_grabber.py
@@ -5,10 +5,17 @@
 # 
 # 
 
+"""
+    Contains methods for stripping URLs out of websites
+"""
+
 import re
 import urllib2
 
 def get_email_from_url(url):
+    """
+       Returns all emails from a url
+    """
     reader = urllib2.urlopen(url)
     emails = set(re.findall('[[\w|\.]+\@\w+\.\w{3}', reader.read()))
     return emails
