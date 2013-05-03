8149632ba70ff700ae04e81e1f730a3be711ac12
Michael J. Gilliland <mjg.py3@gmail.com>
2013-05-03 13:14:23 -0400

    Continued improving lint scores

--- EmailGrabber/email_writer.py
+++ EmailGrabber/email_writer.py
@@ -5,27 +5,49 @@
 # 
 # 
 
+"""
+    Contains writer classes for different outputs
+"""
+
 class EmailFileWriter:
+    """
+        The "interface" for a writer object
+    """
     def __init__(self, name):
         self.output_name = name
 
     def write(self, emails, output_file_name):
+        """
+            A function to write out emails in various formats
+        """
         raise NotImplementedError("Each writer must be able to write!")
 
 class CsvWriter(EmailFileWriter):
+    """
+        Used to write out a simple CSV list
+    """
     def __init__(self):
         EmailFileWriter.__init__(self, ".csv")
 
     def write(self, emails, output_file_name):
+        """
+            Writes the emails out emails separated by commas and newlines
+        """
         with open(output_file_name + self.output_name, 'w') as f:
             for email in emails:
                 f.write(email + ',\n')
 
 class PlainTextWriter(EmailFileWriter):
+    """
+        Writes out emails to a plain text file
+    """
     def __init__(self):
         EmailFileWriter.__init__(self, ".txt")
 
     def write(self, emails, output_file_name):
+        """
+            Writes out emails on newlines
+        """
         with open(output_file_name + self.output_name, 'w') as f:
             for email in emails:
                 f.write(email + '\n')
